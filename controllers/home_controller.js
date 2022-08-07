4f6f3ff6b315bafd0645764a0a820b2eb9b6f56f
Nikhil-C82 <nikhil88c@gmail.com>
2022-08-07 13:36:46 +0530

    Displaying Posts to home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,19 @@
+const Post = require('../models/posts');
+
 module.exports.home = function(req,res){
-    return res.render('home',{
-        title:"Home Codial"
-    });
+
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title:"Home Codial",
+    //         posts:posts
+    //     });
+    // })
+
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts
+        });
+    })
+    
 }
